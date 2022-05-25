b4ade1865eb96b4bba422a73395a5f19e56f6744
Alexandre Soares <alexandre.desousasoares@gmail.com>
2022-05-25 15:02:30 +0300

    adding routes + controller

---
+++ backend/routes/goalsRoutes.js
@@ -0,0 +1,16 @@
+// imports
+const express = require("express");
+const router = express.Router();
+
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+
+// routes
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").put(updateGoal).delete(deleteGoal);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+// imports
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// init express
+const app = express();
+
+app.use("/api/goals", require("./routes/goalsRoutes"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
