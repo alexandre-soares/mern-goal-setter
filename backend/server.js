25b25cf523201ccdb80745fd8dd6914ff1dba448
Alexandre Soares <alexandre.desousasoares@gmail.com>
2022-05-25 15:33:34 +0300

    connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 // imports
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+
+// connect to mongoDB
+connectDB();
 
 // init express
 const app = express();
