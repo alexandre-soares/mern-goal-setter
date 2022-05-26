98f386217384894fc5af5c7608228b9c600ce298
Alexandre Soares <alexandre.desousasoares@gmail.com>
2022-05-26 21:02:19 +0300

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
+// connect to db
 connectDB();
 
 const app = express();
